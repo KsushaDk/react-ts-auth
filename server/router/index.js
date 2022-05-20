59f8eb286acccf60dbcd1a896751b54c80408862
KsushaDk <81408959+KsushaDk@users.noreply.github.com>
2022-05-20 11:19:40 +0300

    add: registration, email activation, error handler

--- server/router/index.js
+++ server/router/index.js
@@ -1,15 +1,16 @@
 const Router = require('express').Router
+const userController = require('../controllers/user-controller')
 const router = new Router()
 
-router.post('/registration')
-router.post('/login')
-router.post('/logout')
+router.post('/registration', userController.registration)
+router.post('/login', userController.login)
+router.post('/logout', userController.logout)
 
 // mail activate
-router.get('/activate/:link')
+router.get('/activate/:link', userController.activate)
 // refresh access token
-router.get('/refresh')
+router.get('/refresh', userController.refresh)
 
-router.get('/users')
+router.get('/users', userController.getUsers)
 
 module.exports = router
---
+++ server/service/user-service.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcrypt')
+const uuid = require('uuid')
+const UserModel = require('../models/user-model')
+const mailService = require('./mail-service')
+const tokenService = require('./token-service')
+const UserDto = require('../dtos/user-dto')
+const ApiError = require('../exceptions/api-error')
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email })
+    if (candidate) {
+      throw ApiError.BadRequest(`User with ${email} already exists`)
+    }
+
+    const hashPassword = await bcrypt.hash(password, 3)
+    const activationLink = uuid.v4()
+
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    })
+
+    await mailService.sendActivationMail(
+      email,
+      `${process.env.API_URL}/api/activate/${activationLink}`,
+    )
+
+    const userDto = new UserDto(user) // id, email, isActivated
+    const tokens = tokenService.generateTokens({ ...userDto })
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+    return { ...tokens, user: userDto }
+  }
+
+  async activate(activationLink) {
+    const user = await UserModel.findOne(activationLink)
+
+    if (!user) {
+      throw ApiError.BadRequest('Activation link is incorrect')
+    }
+
+    user.isActivated = true
+    await user.save()
+  }
+}
+
+module.exports = new UserService()
